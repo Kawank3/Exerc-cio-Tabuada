09005999b128eb03799afbdd8100911ac1556a87
Kawan Weber <kawan_weber@hotmail.com>
2025-06-01 12:33:57 -0300

    Tabuada v1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Digite um número para ver sua tabuada: ");
+int numero = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"\nTabuada do número: {numero}\n");
+
+for (int i = 0; i <= 10; i++)
+{
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
+}
